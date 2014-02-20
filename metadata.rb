6016722af73528281a96cc6f1b9f3f91e2bb8ca6
Gabriel Gayan <gabrielgayan@gmail.com>
2014-02-20 10:43:50 -0300

    state that the cookbook is used with sprout

--- metadata.rb
+++ metadata.rb
@@ -2,8 +2,6 @@ maintainer "Cyril Mougel"
 maintainer_email "cyril.mougel@gmail.com"
 name "oh_my_zsh"
 license "MIT"
-description "A chef recipe to install oh-my-zsh"
+description "A chef recipe to install oh-my-zsh using sprout"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version "0.0.1"
-
-depends 'git'
