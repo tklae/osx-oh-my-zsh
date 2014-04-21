af214e2f57e9df8555db29dc485c02acff7927b3
tklae <github@klaesener.net>
2014-04-21 20:42:57 +0530

    Source .aliases file

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default.oh_my_zsh.theme = 'gnzh'
 default.oh_my_zsh.case_sensitive = false
 default.oh_my_zsh.plugins = ['git','brew','bower','gradle','last-working-dir','mvn','npm','osx','rvm','vagrant','web-search']
-default.oh_my_zsh.sources = []
+default.oh_my_zsh.sources = ['~/.aliases']
